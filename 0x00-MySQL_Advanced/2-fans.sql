f69d144b027d7be318df94192f424ca7d29cfaa0
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-11 21:15:44 +0000

    A new update on the task

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,19 +1,8 @@
--- A SQL script that ranks country origins of bands
+-- a SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
-
-CREATE TABLE IF NOT EXISTS test (
-        origin VARCHAR(255),
-        nb_fans INT(255)
-);      
--- An SQL query statement that ranks country origins of bands
--- ordered by the number of non unique fans.
 
 INSERT INTO test
 SELECT INTO origin, COUNT(*) AS nb_fans
 FROM metal bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
-
--- Display the table
-SELECT origin, nb_fans
-FROM test;
