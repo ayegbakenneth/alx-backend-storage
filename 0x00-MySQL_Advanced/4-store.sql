aa580e267af6eac8db6a9931a50f613f8dd4d959
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-13 11:12:42 +0000

    Trigger concept implementation

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,24 @@
+-- The items table
+CREATE TABLE IF NOT EXISTS items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT DEFAULT 0
+);
+
+-- The orders table
+CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    item_id INT,
+    quantity INT,
+    FOREIGN KEY (item_id) REFERENCES items(id)
+);
 DELIMITER //
 -- SQL script that creates a trigger to adjust another table
 CREATE TRIGGER trig_order
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items
+    UPDATE orders
     SET quantity = quantity - NEW.quantity
     WHERE id = NEW.item_id
 END //
