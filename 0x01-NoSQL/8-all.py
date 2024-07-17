3fe88b50c9f49300d15cd93e03fd430dbfeeb37b
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-17 00:45:56 -0700

    Using python script to lists documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+""" File executable path """
+from typing import List
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """ A function that lists all documents in a collection """
+    mongo_list = []
+    if mongo_collection is None:
+        return []
+    for collection in mongo_collection.find():
+        mongo_list.append(collection)
+    return mongo_list
