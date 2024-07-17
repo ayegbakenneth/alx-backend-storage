b2794749fe8cda309a638311328dbb1e766d5e03
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-17 00:58:39 -0700

    New update

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,9 +6,8 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     """ A function that lists all documents in a collection """
+
     mongo_list = []
-    if mongo_collection is None:
-        return []
     for collection in mongo_collection.find():
         mongo_list.append(collection)
     return mongo_list
